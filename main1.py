df3ff51ccf49c8b8cebd548cf4da9682158258e4
EmmaCimardi <empcc23@gmail.com>
2025-01-23 17:49:00 +0100

    modifiche html, form, edit

--- main1.py
+++ main1.py
@@ -4,8 +4,8 @@ from flask import Flask, request, render_template, redirect, jsonify
 app= Flask(__name__) #nome della flask application
 
 @app.route("/") #/path della risorsa a cui vogliamo accedere
-def home():
-    return "Home";
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
