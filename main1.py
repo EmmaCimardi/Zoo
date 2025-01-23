fcd8dcf95970b7c8e31dea0d75e888a6d3b882b8
EmmaCimardi <empcc23@gmail.com>
2025-01-23 18:38:08 +0100

    cambio nome cartella con file html "templates"

--- main1.py
+++ main1.py
@@ -3,13 +3,16 @@ from flask import Flask, request, render_template, redirect, jsonify
 
 app= Flask(__name__) #nome della flask application
 
-@app.route("/") #/path della risorsa a cui vogliamo accedere
-def index():
-    return render_template("index.html")
+#/path della risorsa a cui vogliamo accedere
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
     
+
+
 #GET
 #POST
 #DELETE
